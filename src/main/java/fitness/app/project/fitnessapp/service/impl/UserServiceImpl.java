d60d5d2c5585870ead6bc2e873e8f3471eb55cda
Nazir Savysko <nazirsavisko@gmail.com>
2025-12-29 22:03:49 +0200

    feat: refactor user retrieval error message and improve HTML/CSS structure for email verification

--- src/main/java/fitness/app/project/fitnessapp/service/impl/UserServiceImpl.java
+++ src/main/java/fitness/app/project/fitnessapp/service/impl/UserServiceImpl.java
@@ -5,6 +5,7 @@ import fitness.app.project.fitnessapp.model.User;
 import fitness.app.project.fitnessapp.repository.UserRepository;
 import fitness.app.project.fitnessapp.service.UserService;
 import lombok.AllArgsConstructor;
+import org.jspecify.annotations.NonNull;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
@@ -15,6 +16,8 @@ import static java.time.LocalDateTime.now;
 @Service
 @AllArgsConstructor
 public final class UserServiceImpl implements UserService {
+    private static final String USER_NOT_FOUND_ERROR = "User with email %s not found";
+
     private final UserRepository userRepository;
     private final PasswordEncoder passwordEncoder;
 
@@ -24,7 +27,7 @@ public final class UserServiceImpl implements UserService {
     }
 
     @Override
-    public User createUser(final String email, final String password) throws UserExistsException {
+    public @NonNull User createUser(final String email, final String password) throws UserExistsException {
        final User user = new User();
        user.setEmail(email);
        user.setPasswordHash(this.passwordEncoder.encode(password));
@@ -43,7 +46,7 @@ public final class UserServiceImpl implements UserService {
     @Override
     public User getUserByEmail(final String email) {
         return this.userRepository.findByEmail(email).orElseThrow(() ->
-                new UserExistsException(format("User with email %s not found", email))
+                new UserExistsException(format(USER_NOT_FOUND_ERROR, email))
         );
     }
 }
