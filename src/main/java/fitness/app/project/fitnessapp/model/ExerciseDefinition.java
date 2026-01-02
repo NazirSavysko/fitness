66c071d795d9c5e8257225a68da73a5b90c72677
Nazir Savysko <nazirsavisko@gmail.com>
2026-01-03 01:50:02 +0200

    feat: enable Hibernate second-level caching for ExerciseDefinition and update repository methods

--- src/main/java/fitness/app/project/fitnessapp/model/ExerciseDefinition.java
+++ src/main/java/fitness/app/project/fitnessapp/model/ExerciseDefinition.java
@@ -5,14 +5,18 @@ import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
+import org.hibernate.annotations.Cache;
 
 import static jakarta.persistence.GenerationType.IDENTITY;
+import static org.hibernate.annotations.CacheConcurrencyStrategy.READ_ONLY;
 
 @AllArgsConstructor
 @NoArgsConstructor
 @Setter
 @Getter
 @Entity
+@Cacheable
+@Cache(usage = READ_ONLY)
 @Table(name = "exercise_definition", schema = "fitness_app")
 public final class ExerciseDefinition {
 
--- src/main/java/fitness/app/project/fitnessapp/repository/ExerciseDefinitionRepository.java
+++ src/main/java/fitness/app/project/fitnessapp/repository/ExerciseDefinitionRepository.java
@@ -1,7 +1,16 @@
 package fitness.app.project.fitnessapp.repository;
 
 import fitness.app.project.fitnessapp.model.ExerciseDefinition;
+import jakarta.persistence.QueryHint;
+import org.jspecify.annotations.NonNull;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.QueryHints;
+
+import java.util.List;
 
 public interface ExerciseDefinitionRepository extends JpaRepository<ExerciseDefinition, Integer> {
+    @Override
+    @QueryHints(@QueryHint(name = "org.hibernate.cacheable", value = "true"))
+    @NonNull
+    List<ExerciseDefinition> findAll();
 }
