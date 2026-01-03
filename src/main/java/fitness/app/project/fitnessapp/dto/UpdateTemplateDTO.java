db4cceb4a0851d0c38fc1a66f1f8f59b9f707dad
Nazir Savysko <nazirsavisko@gmail.com>
2026-01-03 14:20:54 +0200

    feat: add validation for exercises in UpdateTemplateDTO

--- src/main/java/fitness/app/project/fitnessapp/dto/UpdateTemplateDTO.java
+++ src/main/java/fitness/app/project/fitnessapp/dto/UpdateTemplateDTO.java
@@ -1,9 +1,14 @@
 package fitness.app.project.fitnessapp.dto;
 
+import jakarta.validation.Valid;
+import jakarta.validation.constraints.NotEmpty;
+
 import java.util.List;
 
 public record UpdateTemplateDTO(
         Integer id,
         String name,
+        @Valid
+        @NotEmpty(message = "You must add at least one exercise")
         List<TemplateExerciseDTO> exercises
 ) {}
